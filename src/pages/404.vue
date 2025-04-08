b59b78ff30dff8690956bb071375074bf65c6947
Gregory Mitchell <54124162+gmitch215@users.noreply.github.com>
2025-04-08 02:17:28 -0500

    Update 404.vue

--- src/pages/404.vue
+++ src/pages/404.vue
@@ -3,6 +3,6 @@
         <img src="/404.jpg" alt="Page Not Found" class="rounded-lg mb-8 w-64 md:w-96 lg:w-192 xl:w-256 shadow-xl shadow-green-500/30
         hover:scale-110 transition-transform duration-900" />
         <h1 class="text-sm md:text-3xl lg:text-4xl xl:text-5xl mt-4 font-bold">Johnny!</h1>
-        <h3 class="mt-3">We got lost looking for a different page!</h3>
+        <h3 class="mt-3">We got lost looking for a different page.</h3>
     </div>
 </template>
