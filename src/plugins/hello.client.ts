3aba4d97cc3cbb47195ade3cf6d54f101b05f63b
Gregory Mitchell <54124162+gmitch215@users.noreply.github.com>
2026-08-09 01:40:43 -0500

    feat: add poem easter egg

---
+++ src/plugins/hello.client.ts
@@ -0,0 +1,7 @@
+export default defineNuxtPlugin(() => {
+	console.log("%cworld's okayest developer.", 'color:#a2f213;font-weight:bold;font-size:14px');
+	console.log(
+		'%cpoked around in here? have a poem in c -> https://gist.github.com/gmitch215/cf4a7376e66e835c7b8a53b85a8713d5',
+		'color:#83d000'
+	);
+});
